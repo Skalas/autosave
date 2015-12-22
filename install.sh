1d1efc6b9add7e3d9f75264dcbb669dea11e1330
skalas <miguel@opi.la>
2015-12-22 00:57:21 -0600

    IT Works

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ echo "$1"
 sed "s|path|$1|g" "check.sh" > /tmp/check.sh
 /bin/bash /tmp/check.sh &&
     mkdir -p $HOME/autosave/ &&
-    echo "#!/bin/bash" > $HOME/autosave/autosave.sh 
+    echo "#!/bin/bash" > $HOME/autosave/autosave.sh &&
     sed "s|path|$1|g" autosave.sh >> $HOME/autosave/autosave.sh &&
-    chmod +x $HOME/autosave/autosave.sh
-crontab crontab.txt
+    chmod +x $HOME/autosave/autosave.sh &&
+    sed "s|~|$HOME|g" "crontab" > crontab.txt
+
