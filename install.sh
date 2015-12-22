c1a3cdef3b2a3d432a65573cacbddd4f358fb687
skalas <miguel@opi.la>
2015-12-22 01:10:19 -0600

    Make the folder invisible :o

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 echo "$1"
 sed "s|path|$1|g" "check.sh" > /tmp/check.sh
 /bin/bash /tmp/check.sh &&
-    mkdir -p $HOME/autosave/ &&
-    echo "#!/bin/bash" > $HOME/autosave/autosave.sh &&
-    sed "s|path|$1|g" autosave.sh >> $HOME/autosave/autosave.sh &&
-    chmod +x $HOME/autosave/autosave.sh &&
+    mkdir -p $HOME/.autosave/ &&
+    echo "#!/bin/bash" > $HOME/.autosave/autosave.sh &&
+    sed "s|path|$1|g" autosave.sh >> $HOME/.autosave/autosave.sh &&
+    chmod +x $HOME/.autosave/autosave.sh &&
     sed "s|~|$HOME|g" "crontab" > crontab.txt
 
