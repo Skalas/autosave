913e624afc3843acd36f7114801f6424afe6666e
skalas <miguel@opi.la>
2015-12-22 00:49:32 -0600

    First installer

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "$1"
+sed "s|path|$1|g" "check.sh" > /tmp/check.sh
+/bin/bash /tmp/check.sh &&
+    mkdir -p $HOME/autosave/ &&
+    echo "#!/bin/bash" > $HOME/autosave/autosave.sh 
+    sed "s|path|$1|g" autosave.sh >> $HOME/autosave/autosave.sh &&
+    chmod +x $HOME/autosave/autosave.sh
+crontab crontab.txt
